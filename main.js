a3342186123bd3cb13894ee92b3f8e658f9f7888
Indra <Indramohankhatri@gmail.com>
2025-04-20 23:40:10 +0530

    added camera

--- main.js
+++ main.js
@@ -7,10 +7,10 @@ document.addEventListener('DOMContentLoaded',(e) => {
 
 const windowMaker = () => {
     const window = document.createElement('div');
-    window.className = 'flex flex-col h-1/3 w-1/3 absolute left-40 top-10 bg-black';
+    window.className = 'flex flex-col h-3/4 w-2/3 absolute left-40 top-10 bg-black';
 
     const windowButtonsNav = document.createElement('div');
-    windowButtonsNav.className = 'flex h-[10%] justify-end w-full bg-gray-500 items-baseline px-2 items-center'
+    windowButtonsNav.className = 'flex h-[5%] justify-end w-full bg-gray-500 items-baseline px-2 items-center'
 
     const closeBtn = document.createElement('button');
     closeBtn.className = 'w-[1.2rem] h-[1.2rem] flex items-center justify-center rounded-full bg-red-600 text-white hover:bg-red-700 transition'
@@ -58,14 +58,31 @@ const handleCameraOpener = (e) => {
     const window = windowMaker();
 
     if(navigator && navigator.mediaDevices){
-        const options = {audio: false, video: { facingMode: "user", width: 300, height: 300 }};
+        const options = {audio: false, video: { facingMode: "user"}};
         const videoElem = document.createElement('video');
+        videoElem.className = 'w-full h-full scale-x-[-1]';
 
+        navigator.mediaDevices.getUserMedia(options).then((stream) => {
+            videoElem.srcObject = stream;
+            videoElem.onloadedmetadata = function(e) {
+                videoElem.play();
+            };
 
+            window.appendChild(videoElem);
+        }).catch((err) => {
+            const errDiv = document.createElement('div');
+            errDiv.className = 'flex w-full h-full items-center justify-center text-white';
+
+            const text = document.createElement('h4');
+            text.textContent = err;
+
+            errDiv.appendChild(text);
+            window.appendChild(errDiv);
+        })
     }
     else{
         const errDiv = document.createElement('div');
-        errDiv.className = 'flex w-full h-full items-center text-center';
+        errDiv.className = 'flex w-full h-full items-center justify-center text-white';
 
         const text = document.createElement('h4');
         text.textContent = 'CAMERA NOT SUPPORTED :(';
