d1151672af18968165b6eea3c47e26e1ddde0be7
Indra <Indramohankhatri@gmail.com>
2025-07-19 14:53:44 +0530

    Updates

--- assets/index-DD3QlPCk.js
+++ assets/index-DD3QlPCk.js
@@ -88,14 +88,15 @@
             <div class="mb-4">
                 <h3 class="text-lg font-semibold text-gray-900">C++ Interpreter <span class="font-normal text-gray-600">| C++</span></h3>
                 <p class="mb-1">A custom interpreter built entirely from scratch with lexer, parser, and AST design.</p>
-                <p class="mb-1">Supports variables, function declarations, if-then-else control flow, and local scopes. Intended as a hands-on learning tool for compiler/interpreter design.</p>
+                <p class="mb-1">Supports variables, function declarations, if-then-else control flow, and local scopes.</p>
+                <p class="mb-1">Intended as a hands-on learning tool for compiler/interpreter design.</p>
                 <p class="text-md text-blue-600 hover:underline"><a href="https://github.com/PuzzledK/Math-Solver" target="_blank" rel="noopener noreferrer">GitHub: github.com/PuzzledK/Math-Solver</a></p>
             </div>
 
             <div class="mb-4">
                 <h3 class="text-lg font-semibold text-gray-900">Chat-Application <span class="font-normal text-gray-600">| C, Sockets</span></h3>
                 <p class="mb-1">A terminal-based chat application using TCP/IP socket programming in C.</p>
-                <p class="mb-1">Supports multiple client-server messaging through basic threading model. Provides foundation to explore network programming and concurrent communication.</p>
+                <p class="mb-1">Supports multiple client-server messaging through basic threading model.</p>
                 <p class="mb-1">Provides foundation to explore network programming and concurrent communication.</p>
                 <p class="text-md text-blue-600 hover:underline"><a href="https://github.com/PuzzledK/Chat-application-in-C-sockets" target="_blank" rel="noopener noreferrer">GitHub: github.com/PuzzledK/Chat-application-in-C-sockets</a></p>
             </div>
